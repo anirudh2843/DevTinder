c7ca67e8175a8dc1c51cb08107374b1b13789e41
Anirudh <danirud4@gmail.com>
2025-01-29 22:16:55 +0530

    post api call to store data user

--- src/app.js
+++ src/app.js
@@ -3,15 +3,10 @@ const connectDB = require("../config/database");
 const app = express();
 const User = require("../models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Anirudh",
-    lastName: "Domakonda",
-    password: "anirudh",
-    age: "20",
-    gender: "male",
-    emailId: "danirud4@gmail.com",
-  });
+  const user = new User(req.body);
   try {
     user.save();
     res.send("user added successsfully");
